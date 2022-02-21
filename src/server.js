aa5a6237280061e7cf6676eabf9d95ce1b2656ad
Aakash-Naykude <aakashnaykude123@gmail.com>
2022-02-21 13:38:25 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const app = require("./app");
-const connect = require("./configs/db")
+const connect = require("./configs/db");
 app.listen(2300, async function () {
   await connect();
   console.log("listening on 2300 port");
